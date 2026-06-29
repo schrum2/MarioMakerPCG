d7e184a949b6a34f83976f0657797c0820b99b93
Jacob Schrum <schrum2@southwestern.edu>
2026-06-29 13:40:16 -0500

    Smaller batches again
    #25 Lab machines still couldn't handle, so I decreased batch size again, but added gradient accumulation to compensate

--- bat/train-mario-maker-big-unconditional.bat
+++ bat/train-mario-maker-big-unconditional.bat
@@ -15,7 +15,7 @@ set DIFF_OUTPUT=%GAME%-big-unconditional%SEED%
 REM Need to set this! Must be 32x32 if 4 dim_mults are used
 set DATASET=1-_10k_levels_32x32_new.json
 
-python train_diffusion.py --batch_size 16 --model_dim 256 --dim_mults 1 2 4 8 --attention_head_dim 32 --down_block_types "DownBlock2D" "AttnDownBlock2D" "AttnDownBlock2D" "AttnDownBlock2D" --up_block_types "AttnUpBlock2D" "AttnUpBlock2D" "AttnUpBlock2D" "UpBlock2D" --game %GAME% --save_image_epochs 20 --output_dir "%DIFF_OUTPUT%" --num_epochs 500 --json datasets\%DATASET% --seed %SEED%
+python train_diffusion.py --batch_size 8 --gradient_accumulation_steps 2 --model_dim 256 --dim_mults 1 2 4 8 --attention_head_dim 32 --down_block_types "DownBlock2D" "AttnDownBlock2D" "AttnDownBlock2D" "AttnDownBlock2D" --up_block_types "AttnUpBlock2D" "AttnUpBlock2D" "AttnUpBlock2D" "UpBlock2D" --game %GAME% --save_image_epochs 20 --output_dir "%DIFF_OUTPUT%" --num_epochs 500 --json datasets\%DATASET% --seed %SEED%
 
 
 REM call run_diffusion too
